794fd411e0e040771edf9c7a5eb0006983182a76
Ahmed Abd Elnaser <102987038+AhmedAbdElnaser-Dev@users.noreply.github.com>
2026-04-13 08:16:41 +0200

    first problem

---
+++ 643. Maximum Average Subarray I.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving
+{
+    internal class _643
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            var ans = nums.Take(k);
+
+            var avg = ans.Average();
+
+            double max = ans.Average();
+
+            for (int i = k; i < nums.Length; i++)
+            {
+                var temp = avg * k - nums[i - k] + nums[i];
+                avg = temp / k;
+                if (avg > max)
+                {
+                    max = avg;
+                }
+            }
+            return max;
+        }
+    }
+}
